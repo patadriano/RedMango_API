bde6ddb005f65193bdb264327ee4db4aa5a4cb88
patadriano <patriciamae1602@gmail.com>
2025-02-11 15:40:37 +0800

    "179."

---
+++ RedMango_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Azure.Storage.Blobs.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RedMango_API.Data
+{
+    public class ApplicationDbContext : IdentityDbContext 
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
--- RedMango_API/Program.cs
+++ RedMango_API/Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using RedMango_API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    }
+    );
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores < ApplicationDbContext >
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
